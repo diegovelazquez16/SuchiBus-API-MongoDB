fc2be7ae6261ff31fa708093bad35891441ef62c
Diego Velazquez <govelazmen@gmail.com>
2024-12-01 21:54:46 -0600

    Updadate: cors

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const { connectDB } = require('./config');
 const dotenv = require('dotenv');
+const cors = require('cors')
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: 'http://localhost:4200', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 connectDB();
